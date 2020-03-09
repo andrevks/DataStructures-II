940a838b2accf266b5e869750510da4acd482299
andrevks <italiabk@gmail.com>
2020-03-09 13:48:43 +0000

    Error in line 201

--- DataStructuresII/Pointers/DoubleLinkedList/LinkedList.c
+++ DataStructuresII/Pointers/DoubleLinkedList/LinkedList.c
@@ -121,10 +121,11 @@ void * pop(LinkedList *list){
 
         aux = list->tail->next;
 
-        while (aux->next != list->tail) 
-            aux = aux->next;
+        while (aux->next != list->tail) aux = aux->next;
+
 
           aux->next = list->tail->next; 
+          
           void * data = list->tail->data;
 
           free(list->tail);
@@ -141,7 +142,7 @@ void * pop(LinkedList *list){
 void* top(LinkedList *list){
     //Retorna NULL caso a lista esteja fazia
     //Senão retorna o endereço do dado
-    return (isEmpty(list))? NULL: list->tail->data;
+    return (isEmpty(list))?NULL:list->tail->data;
 }
 
 //consulta o primeiro item da lista
@@ -149,24 +150,26 @@ void* first(LinkedList *list){
 
     //Retorna NULL caso a lista esteja fazia
     //Senão retorna o endereço do dado
-    return (isEmpty(list))? NULL: list->tail->next->data;
+    return (isEmpty(list))?NULL:list->tail->next->data;
 }
 //Consulta o último item da lista
 void* last(LinkedList *list){
     //Como o objetivo é retornar o último
     //a função Top é reaproveitada
     return top(list);
+
+    
 }
 
  int add(LinkedList *list, int pos, void *data){
 
 
-        if(pos <= 0) return push(list,data);
+        if(pos <= 0)return push(list,data);
 
         Node * aux = getNodeByPos(list,(pos-1));
         if(aux == NULL)return 0;
 
-        Node * new_node = (Node*) malloc(sizeof(Node));
+        Node * new_node =(Node*) malloc(sizeof(Node));
 
         new_node->data = data;
 
@@ -179,23 +182,22 @@ void* last(LinkedList *list){
 
  }
 
-//  Node* getNodeByPos(LinkedList *list, int pos){
+ Node* getNodeByPos(LinkedList *list, int pos){
 
-//         //Se a lista estiver vazia ou a posição foi maior ou igual ao tamanho
-//         //retorna NULL
-//         if(isEmpty(list) || pos >= list->size) return NULL;
+        //Se a lista estiver vazia ou a posição foi maior ou igual ao tamanho
+        //retorna NULL
+        if(isEmpty(list) || pos >= list->size) return NULL;
 
 
-//         //Se caso não acontecer, continuamos e o aux aponta para o início
-//         Node * aux = list->tail->next;
+        //Se caso não acontecer, continuamos e o aux aponta para o início
+        Node * aux = list->tail->next;
 
-//         //o contador é criado
-//         //enquanto a condição do meio acontecer o loop continua
-//         //Aux aponta para os nós e incrementa o count
+        //o contador é criado
+        //enquanto a condição do meio acontecer o loop continua
+        //Aux aponta para os nós e incrementa o count
 
-//         for(int count = 0; (aux!=list->tail && pos!= count) ;count++, aux = aux->next);
-
-//         return aux;
+        for(int count = 0;(aux!=list->tail && pos!= count);count++, aux = aux->next);
+        return aux;
         
 
-//  }
+ }
---
+++ DataStructuresII/Pointers/DoubleLinkedList/LinkedList.h
@@ -0,0 +1,97 @@
+    #ifndef DataStructure_LinkedList_h
+    #define DataStructure_LinkedList_h
+
+    enum boolean {
+        true = 1, false = 0
+    };
+
+    typedef enum boolean bool;
+    
+    /* */
+    typedef struct Node {
+        void *data;
+        struct Node *next;
+    }Node;
+
+    /*Tail aponta para o final da lista
+    e para informar o tamanho foi criado a variável size 
+    */
+   
+    typedef struct LinkedList {
+        //Node *first;
+        Node *tail;
+        int size;
+    }LinkedList;
+
+    /*Por questões de legidibilidade, algumas funções são reutilizadas*/
+    //no LinkedListTest.c:
+    //Qualquer função que alguem implementar
+    //que retorne boleano e os tipos
+    //das funções sejam iguais 
+    
+
+    //Aqui:
+    //Criando um ponteiro de função 
+    //que recebe como parâmetros dois ponteiros void
+    //retornando bool
+
+    //OBS:
+    //Quando utilizar esse tipo "compare",
+    //suponhe-se que o usuário criou 
+    //essa função com o objetivo de comparar
+    //dois dados, a vantagem é que a biblioteca
+    //serve para qualquer tipo de dado
+    typedef bool (*compare)(void*,void*);
+
+    //Inicializa a lista
+    //Caso fosse 
+    void init(LinkedList *list);
+    int enqueue(LinkedList *list, void *data);
+    void* dequeue(LinkedList *list);
+    void* first(LinkedList *list);
+    void* last(LinkedList *list);
+    int push(LinkedList *list, void *data);
+    void* pop(LinkedList *list);
+    void* top(LinkedList *list);
+    bool isEmpty(LinkedList *list);
+
+    //IndexOf
+    //Devolver a posição baseado no dado
+    //Aqui trabalha somente com o endereço dos dados
+    //quem usar a biblioteca, informa o tipo de dado
+    //Para resolver esse problema é criado um ponteiro
+    //de função para devolver a posição baseado no dado
+
+    //Registrador Contador de instrução, guarda o endereço da próxima instrução
+    //Aquele endereço de memória pode ter o endereço de um dado
+    //ou de uma função
+    int indexOf(LinkedList *list, void *data, compare equal);
+
+    //Quer saber endereço do dado guardado
+    //da posição escolhida
+    void* getPos(LinkedList *list, int pos);
+    
+    //função utilitária internamente na lista
+    //retorna o endereço do Node
+    //pela posição informada 
+    Node* getNodeByPos(LinkedList *list, int pos);
+
+    //inserir na posição dada pelo usuário
+    //deve-se percorrer até a pos-1 para conseguir 
+    //adicionar o novo nó
+    int add(LinkedList *list, int pos, void *data);
+
+    //adiciona a lista de origem na lista destino 
+    //ou seja o tail da lista origem vai no início da lista destino
+    //Depois início do destino aponta para o final da lista origem 
+    //importante incrementar o size na lista de destino 
+    int addAll(LinkedList *listDest, int pos, LinkedList *listSource);
+
+    //remove o elemento baseado na posição
+    void* removePos(LinkedList *list, int pos);
+    bool removeData(LinkedList *list, void *data, compare equal);
+
+    
+   
+   
+    #endif
