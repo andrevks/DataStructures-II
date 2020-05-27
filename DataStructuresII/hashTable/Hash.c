9eb833f7ea0d73b2bd5d8219ae7b267b2b96168f
andrevks <italiabk@gmail.com>
2020-05-27 16:09:57 -0400

    A update in the hash function

--- DataStructuresII/hashTable/Hash.c
+++ DataStructuresII/hashTable/Hash.c
@@ -1,24 +1,16 @@
 #include "Hash.h"
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 
 void initHash(HashStruct *hashStruct){
-
     DoublyLinkedList * aux;
     aux = hashStruct->hashes;
-    //printf("hashStruct in BYTES:%ld\n",sizeof(hashStruct));
-    
-    //printf("Aux in BYTES:%ld\n",sizeof(aux));
     for(int i = 0; i<MAX;i++){
-
         init(aux);
-        //printf("Hashes:%p\n",aux);
         aux++;
-        
     }
-
     hashStruct->size = 0;
 }
 
@@ -27,18 +19,20 @@ bool isHashEmpty(HashStruct *hashStruct){
 }
 
 int hash(char *key){
-    int count = 0;
-    for(; *key != '\0';count+= *key,key++);
-
-    return count % MAX;
+    int charSum = 0;
+    for(int i = 0;*key != '\0';i++){
+        charSum += (*key) * (i+1);
+        key++;
+    }
+    return charSum % MAX;
 }
 
 int put(HashStruct *hashStruct, char *key, void *data, compare equal){
     
     if(!containsKey(hashStruct,key,equal)){
-
         int hashValue = hash(key);
         DoublyLinkedList * matchedList;
+
         matchedList = &hashStruct->hashes[hashValue];
         int sucess = enqueue(matchedList,data);
         if(sucess == 1) hashStruct->size++;
@@ -57,18 +51,18 @@ bool containsKey(HashStruct *hashStruct, char *key, compare equal){
 
     hashValue = hash(key);
     matchedList = &hashStruct->hashes[hashValue];
-
-    //printf("matchedList    -   hashStruct->hashes[hashValue]\n"); 
-    //printf("%p   -       %p\n",matchedList,&hashStruct->hashes[hashValue]);
-
     containsKey = indexOf(matchedList,key,equal); 
     return (containsKey>0)?true:false;    
 }
 
 void showHashStruct(HashStruct *hashStruct,printNode print){
-    
+   
+   DoublyLinkedList * aux;
     for(int i = 0; i<MAX;i++){
         if(hashStruct->hashes[i].size > 0){
+            //printf("&hashStruct->hashes[i]:%s e %f",hashStruct->hashes[i].tail->next->next->data);
+            //aux = hashStruct->hashes[i].tail->next->next->data
+            
             show(&hashStruct->hashes[i],print);
         }
     }
--- DataStructuresII/hashTable/HashTest.c
+++ DataStructuresII/hashTable/HashTest.c
@@ -7,7 +7,6 @@ typedef struct userBanco
 {
     char * nome;
     float * saldo;
-
 }userBanco;
 
 bool compararChaves(void * data,void *key){
@@ -21,7 +20,7 @@ bool compararChaves(void * data,void *key){
 
 void imprimirUser(void * user){
     userBanco * user1 = (userBanco*) user;
-    printf("Nome:%s e Saldo:%f", user1->nome,user1->saldo);    
+    printf("Nome:%s e Saldo:%.2f\n", user1->nome,user1->saldo);    
 }
 
 int main (){
@@ -29,37 +28,34 @@ int main (){
    HashStruct Hash;
    initHash(&Hash);
 
-   userBanco jessica;
-   userBanco joao;
-   userBanco andre;
+   userBanco * jessica =(userBanco*) malloc(sizeof(userBanco));
+   userBanco * joao =(userBanco*) malloc(sizeof(userBanco));
+   userBanco * andre =(userBanco*) malloc(sizeof(userBanco));
 
-   jessica.nome =(char*) malloc(30 * sizeof(char));
-   strcpy(jessica.nome,"jessica");
-   jessica.saldo =(float *) malloc(sizeof(float));
-   *jessica.saldo = 7956,95;
-   
-   joao.nome =(char*) malloc(30 * sizeof(char));
-   strcpy(joao.nome,"jessica");
-   joao.saldo =(float *) malloc(sizeof(float));
-   *joao.saldo = 8567,55;
+   jessica->nome =(char*) malloc(30 * sizeof(char));
+   strcpy(jessica->nome,"andrE");
+   jessica->saldo =(float *) malloc(sizeof(float));
+   jessica->saldo = 7956,95;
+  
+   joao->nome =(char*) malloc(30 * sizeof(char));
+   strcpy(joao->nome,"joao");
+   joao->saldo =(float *) malloc(sizeof(float));
+   joao->saldo = 8567,55;
 
-   andre.nome =(char*) malloc(30 * sizeof(char));
-   strcpy(andre.nome,"andre");
-   andre.saldo =(float *) malloc(sizeof(float));
-   *andre.saldo = 15967,55;
+   andre->nome =(char*) malloc(30 * sizeof(char));
+   strcpy(andre->nome,"andre");
+   andre->saldo =(float *) malloc(sizeof(float));
+   andre->saldo = 15967,55;
 
-
-   printf("jessica.nome:%s e jessica.saldo: R$ %.2f\n",jessica.nome,*jessica.saldo); 
-   printf("joao.nome:%s e joao.saldo: R$ %.2f\n",joao.nome,*joao.saldo); 
-   printf("andre.nome:%s e andre.saldo: R$ %.2f\n",andre.nome,*andre.saldo); 
-   
-   
+   printf("jessica.nome:%s e jessica.saldo: R$ %.2f e key:%d\n",jessica->nome,*jessica->saldo,hash(jessica->nome)); 
+   printf("joao.nome:%s e joao.saldo: R$ %.2f e key:%d\n",joao->nome,joao->saldo,hash(joao->nome)); 
+   printf("andre.nome:%s e andre.saldo: R$ %.2f e key:%d\n",andre->nome,andre->saldo,hash(andre->nome)); 
     
-    printf("PUT:%d\n",put(&Hash,jessica.nome,&jessica,compararChaves));
-    printf("PUT:%d\n",put(&Hash,joao.nome,&joao,compararChaves));
-    printf("PUT:%d\n",put(&Hash,andre.nome,&andre,compararChaves));
+   printf("PUT:%d\n",put(&Hash,jessica->nome,&jessica,compararChaves));
+   printf("PUT:%d\n",put(&Hash,joao->nome,&joao,compararChaves));
+   printf("PUT:%d\n",put(&Hash,andre->nome,&andre,compararChaves));
 
-    showHashStruct(&Hash,imprimirUser);
+   showHashStruct(&Hash,imprimirUser);
 
     
     return 0;
