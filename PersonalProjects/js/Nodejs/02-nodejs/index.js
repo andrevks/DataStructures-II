63d4e02707a6bd6a083f74c0bf7fd7fc643b4852
andrevks <italiabk@gmail.com>
2020-05-29 17:12:00 -0400

    Promise project

--- PersonalProjects/js/Nodejs/02-nodejs/index.js
+++ PersonalProjects/js/Nodejs/02-nodejs/index.js
@@ -3,38 +3,39 @@
 1 - Obter o número de um telefone de um usuário a partir de seu ID
 2 - Obter o endereço do usuário pelo ID
 */
-function getUser(callback){ 
-    //Executa uma função após 1 segundo
-    setTimeout(() => {
+function getUser(callback){ //Executa uma função após 1 segundo setTimeout(() => {
+   setTimeout( ()=>{
         return callback(null,{
-            id:1,
-            nome:'Medina',
-            birth:new Date()
-        })
-    }, 1000);
+                id:1,
+                nome:'Medina',
+                birth:new Date()
+            });
+   }, 1000);
 }
 function getPhone(idUser,callback){
     setTimeout(() =>{
         return callback(null,{
             phone:'999396734',
-            ddd: 11
-        })
-    }, 2000);
-}
+            ddd: 11 
+        });
+    }, 2000); 
+          
+} 
+
 function getAddress(idUser, callback){
     setTimeout(()=>{
         return callback(null,{
-           road:'alves',
-           number: 09
-        })
-    })
+            road:'alves',
+            number: 09
+            })
+    }, 2000)
 }
 
 function userSolver(erro,user){
     console.log(`User: ${user}`);
-}
+} 
 
-getUser(function userSolver(error,usuario){
+getUser(function userSolver(error,user){
     //null || "" || 0 === false
     if(error){
         console.error("Error in USER...", error);
@@ -43,12 +44,12 @@ getUser(function userSolver(error,usuario){
     getPhone(user.id, function phoneSolver(error1,phone){
         
         if(error1){
-            console.error("Error in USER...", error);
+            console.error("Error in PHONE...", error);
             return;
         } 
         getAddress(user.id,function solverAddress(error2, address){
             if(error2){
-                console.error("Error in USER...", error);
+                console.error("Error in ADDRESS...", error);
                 return;
             } 
 
@@ -62,7 +63,4 @@ getUser(function userSolver(error,usuario){
     })
 });
 
-//const phone = getPhone(user.id);
 //const address = getAddress(user.id);
-
-//console.log(`Phone: ${phone}`);
