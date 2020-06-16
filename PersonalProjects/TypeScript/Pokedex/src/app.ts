d8ae005592d77dcb81af10fbc45214ddc2a73a2e
andrevks <italiabk@gmail.com>
2020-06-15 20:48:00 -0400

    Setting up the project

---
+++ PersonalProjects/TypeScript/Pokedex/src/app.ts
@@ -0,0 +1,10 @@
+const container: HTMLElement | any = document.getElementById("app");
+const pokemons: number = 100;
+
+
+interface IPokemon {
+    id: number;
+    name: string;
+    image: string;
+    type: string;
+}
