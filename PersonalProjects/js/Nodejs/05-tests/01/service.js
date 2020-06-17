edd3c3b5af4ebfaa993636d16610045d9e17fd35
andrevks <italiabk@gmail.com>
2020-06-17 18:09:02 -0400

    Unit test to request r2-d2 is completed

--- PersonalProjects/js/Nodejs/05-tests/01/service.js
+++ PersonalProjects/js/Nodejs/05-tests/01/service.js
@@ -1,7 +1,7 @@
 const { get } = require('axios');
 
 const URL = 'https://swapi.dev/api/people';
-
+             
 async function getPeople(name){
     const url = `${URL}/?search=${name}&format=json`
     const result = await get(url);
