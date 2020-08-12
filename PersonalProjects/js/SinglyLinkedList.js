0bae6ce11eac33c3b83d676965f32c5a3db78b1f
Andre Geraldo <39144691+andrevks@users.noreply.github.com>
2020-08-12 16:39:25 -0400

    New implementation of singly linked list
    Push and getElementAt finished

--- PersonalProjects/js/SinglyLinkedList.js
+++ PersonalProjects/js/SinglyLinkedList.js
@@ -1,33 +1,66 @@
 //Singly LinkedList
 
-class ListNode{
+class Node{
   constructor(data){
     this.data = data
     this.next = null
   }
 }
-
 class LinkedList {
-  constructor(head = null){
-    this.head = head
+  constructor(func){
+    this.head = undefined;
+    this.size = 0;
+    //this.equalFunc = func || defaultEq;
+
   }
+  push(data){
+    //head -> undefined
+    //head -> [18] -> null
+    const newNode = new Node(data);
+    if(this.head == undefined){
+      //[data]->null
+      //Head -> [data]->null
+      this.head = newNode;
+    
+    }else{
+      //Head ->[data]->[data]->null
+      let latestNode = this.head;
+      
+      while(latestNode.next){
+        latestNode = latestNode.next;
+      }
+
+      latestNode.next = newNode;
+      
+    }
+    this.size++;
+    return 1;
+  }
+  getElementAt(index){
+    
+    if(index < 0 && index > this.size)return;
+
+    let node = this.head;
+    //Head ->[data]->[data]->null
+    let count = 0;
+    while(node && count < index){
+      node = node.next;
+      count++;  
+    }
+    return node;
+
+  }
+ 
 }
 
-size(){
-  let size = 0 
-  
-}
+
+
 
 //That's my plan
-//[10] -> [25]-> null
-let node = new ListNode(10)
-let node1 = new ListNode(25)
-//Pointer from node to node1 
-node.next = node1
+//[10] -> [25]-> [77] -> null
+let list = new LinkedList();
+console.log(list.push(10))
+console.log(list.push(15))
+// console.log(list.head)
+console.log("getElementAt:",list.getElementAt(1))
 
-//Head points to node[10]
-let list = new LinkedList(node)
-
-
-console.log(list.head.data)//10
-console.log(list.head.next.data)//25
