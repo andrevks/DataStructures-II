f3018474545b54129b9950616b10df1c2cef9dfe
andrevks <italiabk@gmail.com>
2020-08-30 15:51:52 -0400

    initTree done

--- DataStructuresII/redBlackTree/redBlackTree.c
+++ DataStructuresII/redBlackTree/redBlackTree.c
@@ -4,4 +4,12 @@
 
 
 
+TreeNode * initTree(TreeNode * tree, TreeNode * parent){
+    tree = (TreeNode*) malloc(sizeof(TreeNode));   
+    tree->data = 0;
+    tree->left = tree->right = NULL;
+    tree->parent = parent;
+    tree->color = 0;
 
+    return tree;
+}
--- DataStructuresII/redBlackTree/redBlackTree.h
+++ DataStructuresII/redBlackTree/redBlackTree.h
@@ -6,10 +6,33 @@
 
 typedef struct TreeNode{
     void* data;
+    struct TreeNode * parent;
     struct TreeNode *left;
     struct TreeNode *right;
-    int color;
+    int color; // 0 = Negro/Preto, 1 = Rubro
 }TreeNode;
+
+/*
+A red-black tree has the following properties:
+
+1 - Every node in T is either red or black.
+
+2 - The root node of T is black.
+
+3 - Every NULL node is black. (NULL nodes are the leaf nodes. 
+They do not contain any keys. When we search for a key that is not 
+present in the tree, we reach the NULL node.)
+
+4 - If a node is red, both of its children are black. This means no two nodes on a path can be red nodes.
+
+5 - Every path from a root node to a NULL node has the same number of black nodes.
+*/
+
+TreeNode * initTree(TreeNode * tree, TreeNode * parent);    
+
+
+
+    
 
 
 #endif
